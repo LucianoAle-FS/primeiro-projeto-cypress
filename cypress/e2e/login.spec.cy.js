8975967c75dc062e0135304bf41aadbfd8dbb564
Luciano Alexandre <lucianoale.eng@gmail.com>
2025-08-07 11:36:21 -0300

    SelectorList form test login

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,24 +1,30 @@
 describe('Orange HRM Tests', () => {
+
+  const selectorsList = {
+    usernameField: "input[placeholder='Username']",
+    passwordField: "input[placeholder='Password']",
+    submitLoginButton: "button[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialAlert: "div[role='alert']",
+  }
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("input[placeholder='Username']").type('Admin')
-    cy.get("input[placeholder='Password']").type('admin123')
-    cy.get("button[type='submit']").click()
-    cy.location('pathname').should('equals','/web/index.php/dashboard/index')
-    //quando damos somente um get, verificamos se tudo está ocorrendo bem 
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.submitLoginButton).click()
+    cy.location('pathname').should('equals', '/web/index.php/dashboard/index')
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
   })
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("input[placeholder='Username']").type('Test')
-    cy.get("input[placeholder='Password']").type('Test')
-    cy.get("button[type='submit']").click()
-    cy.get("div[role='alert']")
-    //quando damos somente um get, verificamos se tudo está ocorrendo bem 
+    cy.get(selectorsList.usernameField).type('Test')
+    cy.get(selectorsList.passwordField).type('Test')
+    cy.get(selectorsList.submitLoginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
+
   })
 
 })
 
-// colocando no it.skip vc pula o texte
 
 
